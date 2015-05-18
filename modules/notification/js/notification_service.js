625e74b256cd6d1ba46f6bda1bc8467e1fd05580
Steffen Jahr <steffen.jahr@thinktecture.com>
2015-05-18 16:27:33 +0200

    Removed jQuery depency

--- modules/notification/js/notification_service.js
+++ modules/notification/js/notification_service.js
@@ -2,10 +2,8 @@
 /* global window */
 'use strict'; // jshint ignore:line
 
-
 angular.module('lumx.notification', [])
-    .service('LxNotificationService', ['$injector', '$rootScope', '$timeout' , function($injector, $rootScope, $timeout)
-    {
+    .service('LxNotificationService', ['$injector', '$rootScope', '$timeout', function ($injector, $rootScope, $timeout) {
         //
         // PRIVATE MEMBERS
         //
@@ -18,25 +16,21 @@ angular.module('lumx.notification', [])
         //
 
         // private
-        function getElementHeight(elem)
-        {
+        function getElementHeight(elem) {
             return parseFloat(window.getComputedStyle(elem, null).height);
         }
 
         // private
-        function moveNotificationUp()
-        {
+        function moveNotificationUp() {
             var newNotifIndex = notificationList.length - 1;
             notificationList[newNotifIndex].height = getElementHeight(notificationList[newNotifIndex].elem[0]);
-            
+
             var upOffset = 0;
-            
-            for (var idx = newNotifIndex; idx >= 0; idx--)
-            {
-                if (notificationList.length > 1 && idx !== newNotifIndex)
-                {
+
+            for (var idx = newNotifIndex; idx >= 0; idx--) {
+                if (notificationList.length > 1 && idx !== newNotifIndex) {
                     upOffset = 24 + notificationList[newNotifIndex].height;
-                    
+
                     notificationList[idx].margin += upOffset;
                     notificationList[idx].elem.css('marginBottom', notificationList[idx].margin + 'px');
                 }
@@ -44,16 +38,13 @@ angular.module('lumx.notification', [])
         }
 
         // private
-        function deleteNotification(notification)
-        {
+        function deleteNotification(notification) {
             var notifIndex = notificationList.indexOf(notification);
-            
+
             var dnOffset = 24 + notificationList[notifIndex].height;
 
-            for (var idx = 0; idx < notifIndex; idx++)
-            {
-                if (notificationList.length > 1)
-                {
+            for (var idx = 0; idx < notifIndex; idx++) {
+                if (notificationList.length > 1) {
                     notificationList[idx].margin -= dnOffset;
                     notificationList[idx].elem.css('marginBottom', notificationList[idx].margin + 'px');
                 }
@@ -63,144 +54,118 @@ angular.module('lumx.notification', [])
             notificationList.splice(notifIndex, 1);
         }
 
-        function notify(text, icon, sticky, color)
-        {
+        function notify(text, icon, sticky, color) {
             var notificationTimeout;
-            var notification = angular.element('<div/>', {
-                class: 'notification'
-            });
+            var notification = createElement('<div></div>').addClass('notification');
 
-            var notificationText = angular.element('<span/>', {
-                class: 'notification__content',
-                text: text
-            });
+            var notificationText = createElement('<span></span>')
+                .addClass('notification__content');
+            notificationText.html(text);
 
-            if (angular.isDefined(icon))
-            {
-                var notificationIcon = angular.element('<i/>', {
-                    class: 'notification__icon mdi mdi-' + icon
-                });
+            if (angular.isDefined(icon)) {
+                var notificationIcon = createElement('<i></i>')
+                    .addClass('notification__icon mdi mdi-' + icon);
 
                 notification
                     .addClass('notification--has-icon')
                     .append(notificationIcon);
             }
 
-            if (angular.isDefined(color))
-            {
+            if (angular.isDefined(color)) {
                 notification.addClass('notification--' + color);
             }
 
-            notification
-                .append(notificationText)
-                .appendTo('body');
+            notification.append(notificationText);
+
+            var body = getBodyElement()
+                .append(notification);
 
             var data = { elem: notification, margin: 0 };
             notificationList.push(data);
             moveNotificationUp();
 
-            notification.bind('click', function()
-            {
+            notification.on('click', function () {
                 deleteNotification(data);
 
-                if(angular.isDefined(notificationTimeout))
-                {
+                if (angular.isDefined(notificationTimeout)) {
                     $timeout.cancel(notificationTimeout);
                 }
             });
 
-            if (angular.isUndefined(sticky) || !sticky)
-            {
-                notificationTimeout = $timeout(function()
-                {
+            if (angular.isUndefined(sticky) || !sticky) {
+                notificationTimeout = $timeout(function () {
                     deleteNotification(data);
                 }, 6000);
             }
         }
 
-        function success(text, sticky)
-        {
+        function success(text, sticky) {
             notify(text, 'check', sticky, 'green');
         }
 
-        function error(text, sticky)
-        {
+        function error(text, sticky) {
             notify(text, 'alert-circle', sticky, 'red');
         }
 
-        function warning(text, sticky)
-        {
+        function warning(text, sticky) {
             notify(text, 'alert', sticky, 'orange');
         }
 
-        function info(text, sticky)
-        {
+        function info(text, sticky) {
             notify(text, 'information-outline', sticky, 'blue');
         }
-
 
         //
         // ALERT & CONFIRM
         //
 
         // private
-        function buildDialogHeader(title)
-        {
+        function buildDialogHeader(title) {
             // DOM elements
-            var dialogHeader = angular.element('<div/>', {
-                class: 'dialog__header p++ fs-title',
-                text: title
-            });
+            var dialogHeader = createElement('<div></div>')
+                .addClass('dialog__header p++ fs-title');
+            dialogHeader.html(title);
 
             return dialogHeader;
         }
 
         // private
-        function buildDialogContent(text)
-        {
+        function buildDialogContent(text) {
             // DOM elements
-            var dialogContent = angular.element('<div/>', {
-                class: 'dialog__content p++ pt0 tc-black-2',
-                text: text
-            });
+            var dialogContent = createElement('<div></div>')
+                .addClass('dialog__content p++ pt0 tc-black-2');
+            dialogContent.html(text);
 
             return dialogContent;
         }
 
         // private
-        function buildDialogActions(buttons, callback)
-        {
+        function buildDialogActions(buttons, callback) {
             var $compile = $injector.get('$compile');
 
             // DOM elements
-            var dialogActions = angular.element('<div/>', {
-                class: 'dialog__actions'
-            });
+            var dialogActions = createElement('<div></div>')
+                .addClass('dialog__actions');
 
-            var dialogLastBtn = angular.element('<button/>', {
-                class: 'btn btn--m btn--blue btn--flat',
-                text: buttons.ok
-            });
+            var dialogLastBtn = createElement('<button></button>')
+                .addClass('btn btn--m btn--blue btn--flat');
+            dialogLastBtn.html(buttons.ok);
 
             // Cancel button
-            if (angular.isDefined(buttons.cancel))
-            {
+            if (angular.isDefined(buttons.cancel)) {
                 // DOM elements
-                var dialogFirstBtn = angular.element('<button/>', {
-                    class: 'btn btn--m btn--red btn--flat',
-                    text: buttons.cancel
-                });
-
-                // Compilation
+                var dialogFirstBtn = createElement('<button></button>')
+                    .addClass('btn btn--m btn--red btn--flat');
+                dialogFirstBtn.html(buttons.cancel);
                 dialogFirstBtn.attr('lx-ripple', '');
+                // Compilation
                 $compile(dialogFirstBtn)($rootScope);
 
                 // DOM link
                 dialogActions.append(dialogFirstBtn);
 
                 // Event management
-                dialogFirstBtn.bind('click', function()
-                {
+                dialogFirstBtn.on('click', function () {
                     callback(false);
                     closeDialog();
                 });
@@ -214,8 +179,7 @@ angular.module('lumx.notification', [])
             dialogActions.append(dialogLastBtn);
 
             // Event management
-            dialogLastBtn.bind('click', function()
-            {
+            dialogLastBtn.on('click', function () {
                 callback(true);
                 closeDialog();
             });
@@ -223,85 +187,85 @@ angular.module('lumx.notification', [])
             return dialogActions;
         }
 
-        function confirm(title, text, buttons, callback)
-        {
+        function confirm(title, text, buttons, callback) {
             // DOM elements
-            dialogFilter = angular.element('<div/>', {
-                class: 'dialog-filter'
-            });
+            dialogFilter = createElement('<div></div>');
+            dialogFilter.addClass('dialog-filter');
 
-            dialog = angular.element('<div/>', {
-                class: 'dialog dialog--alert'
-            });
+            dialog = createElement('<div></div>');
+            dialog.addClass('dialog dialog--alert');
 
             var dialogHeader = buildDialogHeader(title);
             var dialogContent = buildDialogContent(text);
             var dialogActions = buildDialogActions(buttons, callback);
 
             // DOM link
-            dialogFilter.appendTo('body');
+            var body = getBodyElement().append(dialogFilter);
 
             dialog
                 .append(dialogHeader)
                 .append(dialogContent)
-                .append(dialogActions)
-                .appendTo('body')
-                .show();
+                .append(dialogActions);
+
+            body.append(dialog);
+            dialog.css('display', 'block');
 
             // Starting animaton
-            $timeout(function()
-            {
+            $timeout(function () {
                 dialogFilter.addClass('dialog-filter--is-shown');
                 dialog.addClass('dialog--is-shown');
             }, 100);
         }
 
-        function alert(title, text, button, callback)
-        {
+        function alert(title, text, button, callback) {
             // DOM elements
-            dialogFilter = angular.element('<div/>', {
-                class: 'dialog-filter'
-            });
+            dialogFilter = createElement('<div></div>')
+                .addClass('dialog-filter');
 
-            dialog = angular.element('<div/>', {
-                class: 'dialog dialog--alert'
-            });
+            dialog = createElement('<div></div>')
+                .addClass('dialog dialog--alert');
 
             var dialogHeader = buildDialogHeader(title);
             var dialogContent = buildDialogContent(text);
             var dialogActions = buildDialogActions({ ok: button }, callback);
 
             // DOM link
-            dialogFilter.appendTo('body');
+            var body = getBodyElement()
+                .append(dialogFilter);
 
             dialog
                 .append(dialogHeader)
                 .append(dialogContent)
-                .append(dialogActions)
-                .appendTo('body')
-                .show();
+                .append(dialogActions);
+            body.append(dialog);
+            dialog.css('display', 'block');
 
             // Starting animaton
-            $timeout(function()
-            {
+            $timeout(function () {
                 dialogFilter.addClass('dialog-filter--is-shown');
                 dialog.addClass('dialog--is-shown');
             }, 100);
         }
 
         // private
-        function closeDialog()
-        {
+        function closeDialog() {
             // Starting animaton
             dialogFilter.removeClass('dialog-filter--is-shown');
             dialog.removeClass('dialog--is-shown');
 
             // After animaton
-            $timeout(function()
-            {
+            $timeout(function () {
                 dialogFilter.remove();
                 dialog.remove();
             }, 600);
+        }
+
+        function getBodyElement() {
+            return createElement(document.body);
+        }
+
+        function createElement(element) {
+            return angular.element(element);
         }
 
         // Public API
--- modules/ripple/js/ripple_directive.js
+++ modules/ripple/js/ripple_directive.js
@@ -1,67 +1,69 @@
 /* global angular */
 'use strict'; // jshint ignore:line
 
-
 angular.module('lumx.ripple', [])
-    .directive('lxRipple', ['$timeout', function($timeout)
-    {
+    .directive('lxRipple', ['$window', '$document', '$timeout', function ($window, $document, $timeout) {
         return {
             restrict: 'A',
-            link: function(scope, element, attrs)
-            {
-                var timeout;
+            link: function (scope, element, attrs) {
+                function createElement(element) {
+                    return angular.element(element);
+                }
 
+                function getElementOffset(element) {
+                    var de = $document[0].documentElement;
+                    var box = element.getBoundingClientRect();
+                    var top = box.top + $window.pageYOffset - de.clientTop;
+                    var left = box.left + $window.pageXOffset - de.clientLeft;
+                    return { top: top, left: left };
+                }
+
+                var timeout;
                 element
                     .css({
                         position: 'relative',
                         overflow: 'hidden'
                     })
-                    .bind('mousedown', function(e)
-                    {
+                    .on('mousedown', function (e) {
                         var ripple;
 
-                        if (element.find('.ripple').length === 0)
-                        {
-                            ripple = angular.element('<span/>', {
-                                class: 'ripple'
-                            });
+                        if (element.find('.ripple').length === 0) {
+                            ripple = createElement('<span></span>').addClass('ripple');
 
-                            if (attrs.lxRipple)
-                            {
+                            if (attrs.lxRipple) {
                                 ripple.addClass('bgc-' + attrs.lxRipple);
                             }
 
                             element.prepend(ripple);
                         }
-                        else
-                        {
-                            ripple = element.find('.ripple');
+                        else {
+                            ripple = createElement(element[0].querySelector('.ripple'));
                         }
 
                         ripple.removeClass('ripple--is-animated');
 
-                        if (!ripple.height() && !ripple.width())
-                        {
-                            var diameter = Math.max(element.outerWidth(), element.outerHeight());
+                        var clientRect = ripple[0].getBoundingClientRect();
+
+                        if (!clientRect.height && !clientRect.width) {
+                            var diameter = Math.max(element[0].offsetWidh, element[0].offsetHeight);
 
                             ripple.css({ height: diameter, width: diameter });
                         }
 
-                        var x = e.pageX - element.offset().left - ripple.width() / 2;
-                        var y = e.pageY - element.offset().top - ripple.height() / 2;
+                        var x = e.pageX - getElementOffset(element[0]).left - clientRect.width / 2;
+                        var y = e.pageY - getElementOffset(element[0]).top - clientRect.height / 2;
 
-                        ripple.css({ top: y+'px', left: x+'px' }).addClass('ripple--is-animated');
+                        ripple.css({ top: y + 'px', left: x + 'px' }).addClass('ripple--is-animated');
 
-                        timeout = $timeout(function()
-                        {
+                        timeout = $timeout(function () {
                             ripple.removeClass('ripple--is-animated');
                         }, 651);
                     });
 
-                scope.$on('$destroy', function()
-                {
+                scope.$on('$destroy', function () {
                     $timeout.cancel(timeout);
                 });
+
             }
         };
     }]);
